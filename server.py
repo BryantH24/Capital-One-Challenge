63733d026ca20c325088a93ad7488135b665a187
BryantH24 <gbhou5@gmail.com>
2020-02-22 20:35:01 -0600

    finished google maps and yelp calls

--- server.py
+++ server.py
@@ -5,10 +5,14 @@ import requests
 
 from yelpapi import YelpAPI
 
+from flask_googlemaps import GoogleMaps
 
-api_key = ""
+
+api_key = open('yelpKey.txt', 'r').read()
 #must use static folder for images
 app = Flask(__name__, static_folder=r'\templates\static')
+app.config['GOOGLEMAPS_KEY'] = open('mapsKey.txt', 'r').read()
+GoogleMaps(app)
 
 def getLoc(ipAddress):
     try:
@@ -27,12 +31,17 @@ def startPage():
         resName = request.form.get('resName')
         resLoc = request.form.get('resLoc')
         yelp_api = YelpAPI(api_key)
-        yelpJson = yelp_api.search_query(latitude = locCoor['lat'], longitude = locCoor['lon'])
-        #yelpJson = requests.get(url = r"https://api.yelp.com/v3/businesses/search", headers = header,  params = latLongSend)
-        return '''<h1>The restaurant name is: {}</h1>
-                  <h1>Your Latitiude is: {}</h1>
-                  <h1>Your Longtitiude is: {}</h1>
-                  <h1>Yelp data: {}</h1>'''.format(resName, locCoor['lat'], locCoor['lon'], str(yelpJson))
+        yelpJson = yelp_api.search_query(latitude = locCoor['lat'], longitude = locCoor['lon'], limit = 1)
+
+        # return '''<h1>The restaurant name is: {}</h1>
+        #           <h1>Your Latitiude is: {}</h1>
+        #           <h1>Your Longtitiude is: {}</h1>
+        #           <p>Yelp data: {}</p>
+        #           <div>
+        #           {{googlemap("please_work", lat=32.9636, lng=-96.7468, markers=[(-96.7468, 32.9636)]}}
+        #           </div>
+        #           <img src = {}>'''.format(resName, locCoor['lat'], locCoor['lon'], str(yelpJson), yelpJson['businesses'][0]['image_url'])
+        return render_template('mapPage.html', title= locCoor['lat'])
 
     return render_template('layout.html')
 
