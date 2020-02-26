f7fb0781124349f229043ddd796e87b486509415
BryantH24 <gbhou5@gmail.com>
2020-02-25 22:10:06 -0600

    fixed first page, added input, need to do python

--- server.py
+++ server.py
@@ -10,21 +10,37 @@ from flask_googlemaps import GoogleMaps
 import os
 
 #the number of restuarants displayed
-NUM_REST = 1
+NUM_REST = 5
+
+#initialize app
+#must use static folder for images
+app = Flask(__name__, static_folder=r'\templates\static')
+
+#opening (non-pushed) API key files
+api_key = open('yelpKey.txt', 'r').read()
+app.config['GOOGLEMAPS_KEY'] = open('mapsKey.txt', 'r').read()
+
+#initialize maps
+GoogleMaps(app)
 
 class restaurant():
     def init(self, restNum, yelpJson):
+        restNum = int(restNum)
         self.name = yelpJson['businesses'][restNum]['name']
         self.url = yelpJson['businesses'][restNum]['url']
         self.imgUrl = yelpJson['businesses'][restNum]['image_url']
         self.phoneNum = yelpJson['businesses'][restNum]['phone']
         self.lat = yelpJson['businesses'][restNum]['coordinates']['latitude']
         self.long = yelpJson['businesses'][restNum]['coordinates']['longitude']
-api_key = open('yelpKey.txt', 'r').read()
-#must use static folder for images
-app = Flask(__name__, static_folder=r'\templates\static')
-app.config['GOOGLEMAPS_KEY'] = open('mapsKey.txt', 'r').read()
-GoogleMaps(app)
+        self.category = yelpJson['businesses'][restNum]['categories'][0]['title']
+        self.rate = yelpJson['businesses'][restNum]['rating']
+        self.revNum = yelpJson['businesses'][restNum]['review_count']
+        self.distance = format(yelpJson['businesses'][restNum]['distance'] / 1609, '7.2f')
+        self.price = yelpJson['businesses'][restNum]['price']
+
+resObjs = ["r0", "r1", "r2", "r3", "r4"]
+for res in range(0,NUM_REST):
+    resObjs[res] = restaurant()
 
 def getLoc(ipAddress):
     try:
@@ -41,50 +57,35 @@ def getIP():
        ip = request.remote_addr
     return ip
 
+def initObjs(yelpJson):
+    for counter in range(0,NUM_REST):
+        resObjs[counter].init(counter, yelpJson)
 
 @app.route('/', methods = ['GET', 'POST'])
 def startPage():
-    ipAddress = "64.189.201.73"    #for testing locally
-    #ipAddress = getIP()
+    #ipAddress = "64.189.201.73"    #for testing locally
+    ipAddress = getIP()
     locCoor = getLoc(ipAddress)
-    resName = request.form.get('resName')
-    resLoc = request.form.get('resLoc')
     yelp_api = YelpAPI(api_key)
     yelpJson = yelp_api.search_query(latitude = locCoor['lat'], longitude = locCoor['lon'], limit = NUM_REST, term = '')
-    r1 = restaurant()
-    r1.init(0, yelpJson)
+    initObjs(yelpJson)
     if request.method == 'POST':  #this block is only entered when the form is submitted
         locCoor = getLoc(ipAddress)
         resName = request.form.get('resName')
         resLoc = request.form.get('resLoc')
         yelp_api = YelpAPI(api_key)
         yelpJson = yelp_api.search_query(latitude = locCoor['lat'], longitude = locCoor['lon'], limit = NUM_REST)
-        r1 = restaurant()
-        r1.init(0, yelpJson)
-
-        # return '''<h1>The restaurant name is: {}</h1>
-        #           <h1>Your Latitiude is: {}</h1>
-        #           <h1>Your Longtitiude is: {}</h1>
-        #           <p>Yelp data: {}</p>
-        #           <div>
-        #           {{googlemap("please_work", lat=32.9636, lng=-96.7468, markers=[(-96.7468, 32.9636)]}}
-        #           </div>
-        #           <img src = {}>'''.format(resName, locCoor['lat'], locCoor['lon'], str(yelpJson), yelpJson['businesses'][0]['image_url'])
 
 
-        #return '<h1>{}</h1>'.format(str(ip))
-
-        return render_template('finalPage.html',image = yelpJson['businesses'][0]['image_url'], ipA = ipAddress, latitude = locCoor['lat'], longitude = locCoor['lon'], r1=r1 )
+        return render_template('finalPage.html', ipA = ipAddress, latitude = locCoor['lat'], longitude = locCoor['lon'], r0=resObjs[0], r1=resObjs[1], r2=resObjs[2], r3 = resObjs[3], r4=resObjs[4])
 
         #return render_template('mapPage.html', image = r1.imgUrl, ipA = ipAddress, latitude = locCoor['lat'], longitude = locCoor['lon'])  #original template, do not use
 
-    return render_template('firstPage.html',image = yelpJson['businesses'][0]['image_url'], ipA = ipAddress, latitude = locCoor['lat'], longitude = locCoor['lon'], r1=r1 )
+    return render_template('firstPage.html',image = yelpJson['businesses'][0]['image_url'], ipA = ipAddress, latitude = locCoor['lat'], longitude = locCoor['lon'], r0=resObjs[0], r1=resObjs[1], r2=resObjs[2], r3 = resObjs[3], r4=resObjs[4])
 
 
 # run the app.
 if __name__ == "__main__":
-    # Setting debug to True enables debug output. This line should be
-    # removed before deploying a production app.
     app.debug = True
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
