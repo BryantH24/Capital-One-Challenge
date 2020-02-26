56df9358b9bdc2f1201bc723344aaf075397486a
BryantH24 <gbhou5@gmail.com>
2020-02-26 17:58:59 -0600

    almost done

--- server.py
+++ server.py
@@ -64,7 +64,7 @@ def initObjs(yelpJson):
 @app.route('/', methods = ['GET', 'POST'])
 def startPage():
     ipAddress = "64.189.201.73"    #for testing locally
-    ipAddress = getIP()
+    #ipAddress = getIP()
     locCoor = getLoc(ipAddress)
     yelp_api = YelpAPI(api_key)
     yelpJson = yelp_api.search_query(latitude = locCoor['lat'], longitude = locCoor['lon'], limit = NUM_REST)
