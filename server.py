11e59396c7be223abd1e4cd40e854e76f6a1d403
BryantH24 <gbhou5@gmail.com>
2020-02-27 13:03:53 -0600

    null return testing

--- server.py
+++ server.py
@@ -9,7 +9,7 @@ from flask_googlemaps import GoogleMaps
 
 import os
 
-#the number of restuarants displayed
+#the number of restuarants displayed, can be increased with edits in html
 NUM_REST = 5
 
 #initialize app
@@ -23,9 +23,9 @@ app.config['GOOGLEMAPS_KEY'] = open('mapsKey.txt', 'r').read()
 #initialize maps
 GoogleMaps(app)
 
+#class parses information from json, makes template easier to use
 class restaurant():
     def init(self, restNum, yelpJson):
-        restNum = int(restNum)
         self.name = yelpJson['businesses'][restNum]['name']
         self.url = yelpJson['businesses'][restNum]['url']
         self.imgUrl = yelpJson['businesses'][restNum]['image_url']
@@ -36,12 +36,14 @@ class restaurant():
         self.rate = yelpJson['businesses'][restNum]['rating']
         self.revNum = yelpJson['businesses'][restNum]['review_count']
         self.distance = format(yelpJson['businesses'][restNum]['distance'] / 1609, '7.2f')
-        #self.price = yelpJson['businesses'][restNum]['price']
+        self.price = yelpJson['businesses'][restNum]['price']
 
+#on run, creates array of restaurant objects
 resObjs = ["r0", "r1", "r2", "r3", "r4"]
 for res in range(0,NUM_REST):
     resObjs[res] = restaurant()
 
+#gets location from ip using ip-api call
 def getLoc(ipAddress):
     try:
         response = requests.get("http://ip-api.com/json/{}".format(ipAddress))
@@ -56,36 +58,58 @@ def getIP():
     else:
        ip = request.remote_addr
     return ip
-
+#initializes all objects at same time
 def initObjs(yelpJson):
     for counter in range(0,NUM_REST):
         resObjs[counter].init(counter, yelpJson)
+#makes sure that there are 4 restaurants in yelp output
+def checkResults(yelpJson):
+    try:
+        yelpJson['businesses'][4]['name']
+    except IndexError:
+        return -1
 
 @app.route('/', methods = ['GET', 'POST'])
 def startPage():
-    #ipAddress = "64.189.201.73"    #for testing locally
-    ipAddress = getIP()
-    locCoor = getLoc(ipAddress)
     yelp_api = YelpAPI(api_key)
+    #ipAddress = "64.189.201.73"    #for testing locally
+    ipAddress = getIP()           #for deployment
+    locCoor = getLoc(ipAddress)
     yelpJson = yelp_api.search_query(latitude = locCoor['lat'], longitude = locCoor['lon'], limit = NUM_REST)
     initObjs(yelpJson)
     if request.method == 'POST':  #this block is only entered when the form is submitted
-        distanceIn = 20
+        #read in form values
         queryIn = request.form.get('query')
         locationIn = request.form.get('location')
         distanceIn = request.form.get('distance')
         priceIn = request.form.get('price')
-        yelp_api = YelpAPI(api_key)
-        if not locationIn:
-            yelpJson = yelp_api.search_query(latitude = locCoor['lat'], longitude = locCoor['lon'],limit = NUM_REST, term = queryIn, distance = distanceIn, price = priceIn)
-            initObjs(yelpJson)
-        else:
-            yelpJson = yelp_api.search_query(term = queryIn, location = locationIn, distance = distanceIn, price = priceIn, limit = NUM_REST)
-            initObjs(yelpJson)
-        return render_template('finalPage.html', ipA = ipAddress, term = queryIn, mapR = distanceIn, latitude = yelpJson['region']['center']['latitude'], longitude = yelpJson['region']['center']['longitude'], r0=resObjs[0], r1=resObjs[1], r2=resObjs[2], r3 = resObjs[3], r4=resObjs[4])
-
-
-    return render_template('firstPage.html', ipA = ipAddress, latitude = locCoor['lat'], longitude = locCoor['lon'], r0=resObjs[0], r1=resObjs[1], r2=resObjs[2], r3 = resObjs[3], r4=resObjs[4])
+        #search with form values
+        yelpJson = yelp_api.search_query(term = queryIn, location = locationIn, distance = distanceIn, price = priceIn, limit = NUM_REST)
+        if checkResults(yelpJson)==-1: #if the search is too specific
+            return '<h1> Your search did not return enough restaurants to display, please try again</h1><a href = "/" class = "w3-xlarge w3-button ">Clear Search</a>'
+        initObjs(yelpJson)
+        #render page after search using values from form
+        return render_template('finalPage.html',
+                                ipA = ipAddress,
+                                term = queryIn,
+                                mapR = distanceIn,
+                                latitude = yelpJson['region']['center']['latitude'],
+                                longitude = yelpJson['region']['center']['longitude'],
+                                r0=resObjs[0],
+                                r1=resObjs[1],
+                                r2=resObjs[2],
+                                r3 = resObjs[3],
+                                r4=resObjs[4])
+    #render page before search with defaults and ip location
+    return render_template('firstPage.html',
+                            ipA = ipAddress,
+                            latitude = locCoor['lat'],
+                            longitude = locCoor['lon'],
+                            r0=resObjs[0],
+                            r1=resObjs[1],
+                            r2=resObjs[2],
+                            r3 = resObjs[3],
+                            r4=resObjs[4])
 
 
 # run the app.
